5b9b9da60ecabf213ffa0f5ecba1b668026bc3de
CIDRA4023 <cidra3204@gmail.com>
2021-07-10 20:38:54 +0900

    add: OptionMenu(プライバシーポリシーと利用規約の追加) add: ActionBar change: Design

--- app/src/main/java/com/cidra/hologram/MainActivity.kt
+++ app/src/main/java/com/cidra/hologram/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.cidra.hologram
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         instance = this
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.item_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /**
+     * オプションメニューのタップ後処理
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            // プライバシーポリシー
+            R.id.policy -> {
+                val policyUrl = "https://github.com/CIDRA4023/Hologram/blob/master/PrivacyPolicy.md"
+                val intentPolicy = Intent(Intent.ACTION_VIEW)
+                intentPolicy.data = Uri.parse(policyUrl)
+                startActivity(intentPolicy)
+            }
+            // 利用規約
+            R.id.terms -> {
+                val termsUrl = "https://github.com/CIDRA4023/Hologram/blob/master/Terms.md"
+                val intentTerms = Intent(Intent.ACTION_VIEW)
+                intentTerms.data = Uri.parse(termsUrl)
+                startActivity(intentTerms)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
