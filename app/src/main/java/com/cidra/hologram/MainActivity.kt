5711da0c2301ec1822aad69b4d05a73a42dceaab
CIDRA4023 <cidra3204@gmail.com>
2021-08-14 19:31:57 +0900

    add: In-app Updateの実装

--- app/src/main/java/com/cidra/hologram/MainActivity.kt
+++ app/src/main/java/com/cidra/hologram/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.cidra.hologram
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         lateinit var instance: MainActivity
+        val MY_REQUEST_CODE = 100
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +22,55 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         instance = this
         setSupportActionBar(toolbar)
+
+        updateCheck()
+    }
+
+    private fun updateCheck() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        val listener = InstallStateUpdatedListener { state ->
+            // ダウンロード後インストール
+            if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                appUpdateManager.completeUpdate()
+            }
+        }
+        // アップデート状態の確認
+        appUpdateManager.registerListener(listener)
+
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                //&& appUpdateInfo.updatePriority() >= 4
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)
+            ) {
+                Log.i("updateTAG", "update available")
+                // Request the update.
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateInfo,
+                    AppUpdateType.FLEXIBLE,
+                    this,
+                    MY_REQUEST_CODE
+                )
+            } else {
+                Log.i("updateTAG", "no update available")
+            }
+        }.addOnFailureListener {
+            Log.i("error", "$it")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                Log.e("MY_APP", "Update flow failed! Result code: $resultCode")
+                // If the update is cancelled or fails,
+                // you can request to start the update again.
+            }
+            return
+        }
     }
 
 }
